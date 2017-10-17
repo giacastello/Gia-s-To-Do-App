76c34b99df952af990287433c95e4e95a0930ce5
Gia Castello <giacastello@gmail.com>
2017-10-16 23:38:13 -0300

    adding V2, V3

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var notasMusicales = [];
+
+notasMusicales [0] = "Do";
+notasMusicales [1] = "Re";
+notasMusicales [2] = "Mi";
+notasMusicales [3] = "Fa";
+notasMusicales [4] = "Sol";
+notasMusicales [5] = "La";
+notasMusicales [6] = "Si";
+
+var cant = notasMusicales.length; //te va a dar la cantidad de elementos en el array.
+var ultimoElemento = notasMusicales.length-1; //te da la casita del último elemento
+console.log("cantidad de notas:" + ultimoElemento);
+console.log("El último elemento es: " + notasMusicales[notasMusicales.length-1]);
+
+
+//hacer array vacío
+//cargarlo
+//imprimir el primero y último
+
+
+var animalesMitológicos = [];
+
+animalesMitológicos [0] = "Unicornio";
+animalesMitológicos [1] = "Dragón Dorado";
+animalesMitológicos [2] = "Sirena";
+animalesMitológicos [3] = "Ave Fénix";
+animalesMitológicos [4] = "Medusa";
+animalesMitológicos [5] = "Hombre Lobo";
+animalesMitológicos [6] = "Minotauro";
+
+var cant = animalesMitológicos.length; //te va a dar la cantidad de elementos en el array.
+var ultimoElemento = animalesMitológicos.length-1; //te da la casita del último elemento
+console.log("Cantidad de animales mitológicos copados:" + ultimoElemento);
+console.log("El más cool es: " + animalesMitológicos[0]);
+console.log("El menos cool es: " + animalesMitológicos[animalesMitológicos.length-1]);
---
+++ to-dosV4.js
@@ -0,0 +1,33 @@
+var todoList = {
+  // todoList.addTodo should add objects (to store more info, like completed)
+  todos: [], // We leave it blank so we can give functions as objects
+  displayTodos: function() { // it should have a displayTodos method
+    console.log('My Todos:', this.todos);
+  },
+
+  //it should have an addTodo Method
+  addTodo: function(todoText) {
+    this.todos.push({ //objects go in curlies.
+      todoText: todoText, //first: name of property in object- second: parametre.
+      completed: false //boolean true-false. False as default.
+      });
+    this.displayTodos();
+  },
+
+  //todoList.changeTodo should change the todoText property
+  changeTodo: function(position, todoText) {
+    /*  */
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position,1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+
+};
