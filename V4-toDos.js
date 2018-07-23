4e546ad3757fa4a807ec24d12785b91537f4df90
Gia Castello <giacastello@gmail.com>
2018-07-23 01:04:37 -0300

    rename folders

--- V4-toDos.js
+++ V4-toDos.js
@@ -1,29 +1,29 @@
 var todoList = { // todoList.addTodo should add objects (to store more info, like completed)
   todos: [], // We leave it blank so we can give functions as objects
-  displayTodos: function() { // it should have a displayTodos method
+  displayTodos: function () { // it should have a displayTodos method
     console.log('My Todos:', this.todos);
   },
 
   //it should have an addTodo Method
-  addTodo: function(todoText) {
+  addTodo: function (todoText) {
     this.todos.push({ //objects go in curlies.
       todoText: todoText, //first: name of property in object- second: parametre.
       completed: false //boolean true-false. False as default.
-      });
+    });
     this.displayTodos();
   },
 
   //todoList.changeTodo should change the todoText property
-  changeTodo: function(position, todoText) {
+  changeTodo: function (position, todoText) {
     /*  */
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
-  deleteTodo: function(position) {
-    this.todos.splice(position,1);
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
     this.displayTodos();
   },
-  toggleCompleted: function(position) {
+  toggleCompleted: function (position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
