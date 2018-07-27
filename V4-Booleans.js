003066ca5f2cd504acfcbfa2a1238fbb142d203c
Gia Castello <giacastello@gmail.com>
2018-07-27 06:46:17 -0300

    Bang

--- V4-Booleans.js
+++ V4-Booleans.js
@@ -6,18 +6,18 @@ var todoList = { // todoList.addTodo should add objects (to store more info, lik
   },
 
   //it should have an addTodo Method
-  addTodo: function (todoText) {
+  addTodo: function (newTodo) {
     this.todos.push({ //objects go in curlies.
-      todoText: todoText, //first: name of property in object- second: parametre.
+      todoText: newTodo, //first: name of property in object- second: parametre.
       completed: false //boolean true-false. False as default.
     });
     this.displayTodos();
   },
 
   //todoList.changeTodo should change the todoText property
-  changeTodo: function (position, todoText) {
+  changeTodo: function (position, newText) {
     /*  */
-    this.todos[position].todoText = todoText;
+    this.todos[position].newText = newTodo;
     this.displayTodos();
   },
   deleteTodo: function (position) {
@@ -26,8 +26,7 @@ var todoList = { // todoList.addTodo should add objects (to store more info, lik
   },
   toggleCompleted: function (position) {
     var todo = this.todos[position];
-    todo.completed = !todo.completed;
+    todo.completed = !todo.completed; // This is a bang operator and we use it to switch true to false statements back and forth.
     this.displayTodos();
   }
-
 };
